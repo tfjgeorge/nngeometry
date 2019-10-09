75cd136d9f82ec551febc34c78dfc78159bbc3ef
Thomas George <tfjgeorge@gmail.com>
2019-10-09 15:10:56 -0400

    adds kronecker product

---
+++ tests/test_utils.py
@@ -0,0 +1,19 @@
+import torch
+from nngeometry.utils import kronecker
+
+def test_kronecker():
+    eps = 1e-5
+
+    A = torch.tensor([[1., 0.]])
+    B = torch.tensor([[1., 2.],
+                      [3., 4.]])
+    AkronB = torch.tensor([[1., 2., 0., 0.],
+                           [3., 4., 0., 0.]])
+    assert torch.norm(kronecker(A, B) - AkronB) < eps
+
+    A = torch.tensor([[1.], [2.]])
+    AkronB = torch.tensor([[1., 2.],
+                           [3., 4.],
+                           [2., 4.],
+                           [6., 8.]])
+    assert torch.norm(kronecker(A, B) - AkronB) < eps
