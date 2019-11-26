d887e1a98c88af9652686ac8dfe62109abf7e91a
Thomas George <tfjgeorge@gmail.com>
2019-11-26 10:57:46 -0500

    pep8

--- nngeometry/utils.py
+++ nngeometry/utils.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn.functional as F
 
+
 def get_n_parameters(model):
     return sum([p.numel() for p in model.parameters()])
+
 
 def get_individual_modules(model):
     mods = []
@@ -31,10 +33,13 @@ def get_individual_modules(model):
     assert len(set(parameters) - set(model.parameters())) == 0
     return mods, p_pos
 
+
 def per_example_grad_conv(mod, x, gy):
     ks = (mod.weight.size(2), mod.weight.size(3))
     gy_s = gy.size()
     bs = gy_s[0]
-    x_unfold = F.unfold(x, kernel_size=ks, stride=mod.stride, padding=mod.padding, dilation=mod.dilation)
+    x_unfold = F.unfold(x, kernel_size=ks, stride=mod.stride,
+                        padding=mod.padding, dilation=mod.dilation)
     x_unfold_s = x_unfold.size()
-    return torch.bmm(gy.view(bs, gy_s[1], -1), x_unfold.view(bs, x_unfold_s[1], -1).permute(0, 2, 1))
+    return torch.bmm(gy.view(bs, gy_s[1], -1),
+                     x_unfold.view(bs, x_unfold_s[1], -1).permute(0, 2, 1))
