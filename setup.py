be33b6fbbdd870fbb66d4c8c27d79a137775ecc1
Thomas George <tfjgeorge@gmail.com>
2020-03-25 14:49:43 -0400

    updates library url in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='nngeometry',
       version='0.1',
       description='Manipulate geometry matrices in Pytorch',
-      url='#',
+      url='https://github.com/tfjgeorge/nngeometry',
       author='tfjgeorge',
       author_email='tfjgeorge@gmail.com',
       license='MIT',
