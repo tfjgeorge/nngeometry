83fe96d28fc8a91d357b318c185b88e0b203f4a9
Thomas George <tfjgeorge@gmail.com>
2020-04-14 09:39:25 -0400

    adds object __init__.py

---
+++ nngeometry/object/__init__.py
@@ -0,0 +1,7 @@
+from .pspace import (PSpaceDense, PSpaceBlockDiag, PSpaceDiag,
+                     PSpaceLowRank, PSpaceImplicit,
+                     PSpaceKFAC, PSpaceEKFAC)
+from .vector import (PVector, FVector)
+from .fspace import (FSpaceDense,)
+from .map import (PushForwardDense, PushForwardImplicit,
+                  PullBackDense)
