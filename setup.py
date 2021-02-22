27aa8c526ba941c45b73fc479dd2323505eeb91a
Thomas George <tfjgeorge@gmail.com>
2021-02-22 15:28:54 -0500

    Updates version number in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nngeometry',
-      version='0.1',
+      version='0.2.1',
       description='Manipulate geometry matrices in Pytorch',
       url='https://github.com/tfjgeorge/nngeometry',
       author='tfjgeorge',
