6f5fd0613238834d0fcaec760aa20d3772ff59ea
Thomas George <tfjgeorge@gmail.com>
2021-09-24 09:56:38 -0400

    adds weightnorm1d without affine parameters

--- nngeometry/layers.py
+++ nngeometry/layers.py
@@ -1,5 +1,5 @@
 from torch import Tensor
-from torch.nn import Linear
+from torch.nn import Linear, Module
 from torch.nn import functional as F
 import torch
 
@@ -8,7 +8,28 @@ class Cosine(Linear):
     and the incoming data
     """
 
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super(Cosine, self).__init__(in_features=in_features,
+                                     out_features=out_features,
+                                     bias=False)
+
     def forward(self, input: Tensor) -> Tensor:
         return F.linear(input / torch.norm(input, dim=1, keepdim=True),
-                        self.weight / torch.norm(self.weight, dim=1, keepdim=True),
-                        self.bias)
+                        self.weight / torch.norm(self.weight, dim=1, keepdim=True))
+
+
+class WeightNorm1d(Linear):
+    """Computes an affine mapping of the incoming data using a weight matrix
+    with rows normalized with norm 1
+    """
+
+    def __init__(self, in_features: int, out_features: int) -> None:
+        super(WeightNorm1d, self).__init__(in_features=in_features,
+                                     out_features=out_features,
+                                     bias=False)
+
+    def forward(self, input: Tensor) -> Tensor:
+        return F.linear(input,
+                        self.weight / torch.norm(self.weight, dim=1, keepdim=True))
+
+# class Affine(Module):
--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,20 +1,47 @@
-from nngeometry.layers import Cosine
+from nngeometry.layers import Cosine, WeightNorm1d
 import torch
 from utils import check_ratio
 
 def test_cosine():
     cosine_layer = Cosine(2, 3)
 
-    # extract vector parrallel to cosine_layer.weight[0, :]
+    # extract vector parallel to cosine_layer.weight[0, :]
     x_0 = cosine_layer.weight[0, :]
     x_col = 3 * x_0
     # extract vector orthogonal to cosine_layer.weight[1, :]
     x_1 = cosine_layer.weight[1, :]
     x_2 = cosine_layer.weight[2, :]
     x_orth = x_2 - (x_2 * x_1).sum() / torch.norm(x_1)**2 * x_1
+    x_orth *= 1.5
     x = torch.stack((x_col, x_orth))
-    
+
     out = cosine_layer(x)
 
-    check_ratio(1 + cosine_layer.bias[0], out[0, 0])
-    check_ratio(cosine_layer.bias[1], out[1, 1])
+    # this one is parallel hence cos(angle) = 1
+    check_ratio(1, out[0, 0])
+    # this one is orthogonal hence cos(angle) = 0
+    check_ratio(0, out[1, 1])
+
+
+def test_weightnorm1d():
+    weightnorm_layer = WeightNorm1d(2, 3)
+    
+    mult0 = 3.3
+    mult1 = 1.4
+
+    # extract vector parallel to weightnorm_layer.weight[0, :]
+    x_0 = weightnorm_layer.weight[0, :]
+    x_col = mult0 * x_0
+    # extract vector orthogonal to weightnorm_layer.weight[1, :]
+    x_1 = weightnorm_layer.weight[1, :]
+    x_2 = weightnorm_layer.weight[2, :]
+    x_orth = x_2 - (x_2 * x_1).sum() / torch.norm(x_1)**2 * x_1
+    x_orth *= mult1
+    x = torch.stack((x_col, x_orth))
+
+    out = weightnorm_layer(x)
+
+    # this one is parallel hence cos(angle) = 1
+    check_ratio(mult0 * torch.norm(x_0), out[0, 0])
+    # this one is orthogonal hence cos(angle) = 0
+    check_ratio(0, out[1, 1])
