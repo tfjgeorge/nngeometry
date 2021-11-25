36d6e027e509b89849113d987b2ab2309f48ad0a
Thomas George <tfjgeorge@gmail.com>
2021-11-25 11:41:10 -0500

    adds efficient per_example grads for convs

--- nngeometry/utils.py
+++ nngeometry/utils.py
@@ -3,17 +3,6 @@ import torch.nn.functional as F
 from nngeometry.object.vector import PVector
 
 
-def per_example_grad_conv(mod, x, gy):
-    ks = (mod.weight.size(2), mod.weight.size(3))
-    gy_s = gy.size()
-    bs = gy_s[0]
-    x_unfold = F.unfold(x, kernel_size=ks, stride=mod.stride,
-                        padding=mod.padding, dilation=mod.dilation)
-    x_unfold_s = x_unfold.size()
-    return torch.bmm(gy.view(bs, gy_s[1], -1),
-                     x_unfold.view(bs, x_unfold_s[1], -1).permute(0, 2, 1))
-
-
 def display_correl(M, axis):
 
     M = M.get_dense_tensor()
