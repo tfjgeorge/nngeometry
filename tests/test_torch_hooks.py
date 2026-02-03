df1687f9bed2da25cfd6c43251af6ce75c839dd5
Pierre Nodet <pierre.nodet@orange.com>
2026-02-03 23:54:00 +0100

    more iterations and lower eps in solve cg test

--- tests/test_torch_hooks.py
+++ tests/test_torch_hooks.py
@@ -732,8 +732,10 @@ def test_jacobian_pimplicit_vs_pdense():
             )  # good init
             check_tensors(
                 PMat_dense.solve(dw, regul=regul).to_torch(),
-                PMat_implicit.solve(dw, regul=regul, max_iter=10, M=PMat_bd).to_torch(),
-                eps=5e-1,
+                PMat_implicit.solve(
+                    dw, regul=regul, max_iter=1000, M=PMat_bd, x0=dw, rtol=1e-2
+                ).to_torch(),
+                eps=1e-2,
             )  # worse preconditioner
 
 
