de32b1062e60f7ba81dc83f270fc6d39d87b6501
Pierre Nodet <pierre.nodet@orange.com>
2026-08-03 15:34:56 +0200

    add public sqrt_var functions and add EF helper and change function argument in Hessian

--- nngeometry/__init__.py
+++ nngeometry/__init__.py
@@ -1,6 +1,6 @@
-from .metrics import FIM, FIM_MonteCarlo
+from .metrics import FIM, FIM_MonteCarlo, EF
 from .hessian import Hessian
 from .jacobian import Jacobian
 from .gram import GramMatrix
 
-__all__ = ["FIM", "FIM_MonteCarlo", "Hessian", "Jacobian", "GramMatrix"]
+__all__ = ["FIM", "FIM_MonteCarlo", "EF", "Hessian", "Jacobian", "GramMatrix"]
